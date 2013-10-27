bebf8ad99bba01bcf2c3006c03e8af512b0601cc
Singanallur Venkatakrishnan <svenkata@cvrsvc01.(none)>
2013-10-27 12:22:31 -0700

    3D : Changes to Carver recons script file.

--- 3D_Recon_Launcher_for_Real_Data/Carver_Sub_Recon.sh
+++ 3D_Recon_Launcher_for_Real_Data/Carver_Sub_Recon.sh
@@ -3,7 +3,7 @@
 #PBS -q regular
 #PBS -l nodes=1:ppn=1
 #PBS -l pvmem=20GB
-#PBS -l walltime=02:00:00
+#PBS -l walltime=01:00:00
 #PBS -N mpi_test
 #PBS -e my_job.$PBS_JOBID.err
 #PBS -o my_job.$PBS_JOBID.out
@@ -20,4 +20,5 @@ module load mpi4py
 
 setenv OMP_NUM_THREADS 8
 
-python XT_MBIR_3D.py --setup_launch_folder --run_reconstruction --Carver --input_hdf5 $GSCRATCH/LBNL_Datasets/20131004_013841_parikh_soil_microaggregate_1-2_0-15.h5 --code_launch_folder $GSCRATCH/Recon_Runs/LBNL_Recons/XT_run/ --output_hdf5 $GSCRATCH/Results/Carver/ --pix_size 0.65 --num_views 1024 --x_width 2560 --recon_x_width 1024 --z_start 1000 --z_numElts 32 --num_nodes 1 --num_threads 8 --rot_center 1276 --smoothness .024 --view_subsmpl_fact 4 --num_bright_dark 30
+python XT_MBIR_3D.py --setup_launch_folder --run_reconstruction --Carver --input_hdf5 $GSCRATCH/LBNL_Datasets/20130110_235013_km3_peeled1_x0y0.h5 --code_launch_folder $GSCRATCH/Recon_Runs/LBNL_Recons/XT_run/ --output_hdf5 $GSCRATCH/Results/Carver/ --pix_size 3.33 --num_views 2048 --x_width 2560 --recon_x_width 2560 --z_start 200 --z_numElts 3 --num_nodes 1 --num_threads 8 --rot_center 1279 --smoothness 1.1 --view_subsmpl_fact 8 --num_bright_dark 20 --multires_2D --num_res 5
+
