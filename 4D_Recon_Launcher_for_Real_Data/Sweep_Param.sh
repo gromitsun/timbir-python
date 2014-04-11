417e516fea665127724450b50218ad1dec6b282b
Kadri Aditya Mohan <mohank@conte-fe03.rcac.purdue.edu>
2014-04-11 16:16:08 -0400

    Init values for Dataset_21

--- 4D_Recon_Launcher_for_Real_Data/Sweep_Param.sh
+++ 4D_Recon_Launcher_for_Real_Data/Sweep_Param.sh
@@ -1,8 +1,8 @@
 param_idx=`seq 1 1`
 for i in $param_idx; do
 	export PARAM_INDEX=$i
-	qsub -N Dataset_03_Res1040 -v PARAM_INDEX Single_Node_Job.sub 
-   #    qsub -N K_32_R_32_X800_ProjStart24Kset14 -v PARAM_INDEX Multi_Node_Job.sub 
+#	qsub -N z_270 -v PARAM_INDEX Single_Node_Job.sub 
+        qsub -N z_270 -v PARAM_INDEX Multi_Node_Job.sub 
 	sleep 0.2
 done 
 
