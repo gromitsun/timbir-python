735d39bb5332371769f2342ebd3c534e67355fb4
Kadri Aditya Mohan <mohank@conte-fe03.rcac.purdue.edu>
2014-04-12 08:56:33 -0400

    Fixing result folder name in XT_IOMisc.py

--- 4D_Recon_Launcher_for_Real_Data/XT_IOMisc.py
+++ 4D_Recon_Launcher_for_Real_Data/XT_IOMisc.py
@@ -66,7 +66,7 @@ def write_tiff_from_object_bin_file (proj, recon, files):
 	Object = np.zeros((recon['N_z'], recon['N_xy'], recon['N_xy']), dtype = np.float64, order = 'C')
 	for i in range(len(recon['r'])):
                 path2launch = files['Launch_Folder'] + 'run_' + 'sigs_' + str(recon['sigma_s'][i]) + '_sigt_' + str(recon['sigma_t'][i]) + '_r_' + str(recon['r'][i]) + '_K_' + str(proj['K']) + '_N_theta_' + str(proj['N_theta']) + '_N_p_' + str(proj['recon_N_p']) + recon['msg_string'] + '/'
-                path2results = files['Result_Folder'] + 'run_' + 'sigs_' + str(recon['sigma_s'][i]) + '_sigt_' + str(recon['sigma_t'][i]) + '_r_' + str(recon['r'][i]) + '_K_' + str(proj['K']) + '_N_theta_' + str(proj['N_theta']) + '_N_p_' + str(proj['recon_N_p']) + recon['msg_string'] + '/'
+                path2results = files['Result_Folder'] + 'MBIR_' + 'sigs_' + str(recon['sigma_s'][i]) + '_sigt_' + str(recon['sigma_t'][i]) + '_r_' + str(recon['r'][i]) + '_K_' + str(proj['K']) + '_N_theta_' + str(proj['N_theta']) + '_N_p_' + str(proj['recon_N_p']) + recon['msg_string'] + '/'
 		for j in range(int(recon['Rtime_num'][i])):
 			for k in range(recon['node_num']):
 				zpernode = recon['N_z']/recon['node_num']	
