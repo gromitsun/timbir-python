Fixed a N_p overflow bug in python script. The code wasn't predicting the number of cycles (frames) in the data correctly

4D_Recon_Launcher_for_Real_Data/XT_IOMisc.py
@@ -84,23 +84,25 @@ def write_object2HDF (proj, recon, files):
         for i in range(len(recon['r'])):
                 path2results = files['Result_Folder'] + 'MBIR_' + 'sigs_' + str(recon['sigma_s'][i]) + '_sigt_' + str(recon['sigma_t'][i]) + '_r_' + str(recon['r'][i]) + '_K_' + str(proj['K']) + '_N_theta_' + str(proj['N_theta']) + '_N_p_' + str(proj['recon_N_p']) + recon['msg_string'] + '/'
                 path2launch = files['Launch_Folder'] + 'run_' + 'sigs_' + str(recon['sigma_s'][i]) + '_sigt_' + str(recon['sigma_t'][i]) + '_r_' + str(recon['r'][i]) + '_K_' + str(proj['K']) + '_N_theta_' + str(proj['N_theta']) + '_N_p_' + str(proj['recon_N_p']) + recon['msg_string'] + '/'
-                file = h5py.File(path2results + 'object.hdf5', 'w');
-#               dset = file.create_dataset('object', (recon['Rtime_num'][i], recon['N_z'], recon['N_xy'], recon['N_xy']), dtype=np.float32, chunks=True, compression='lzf');
-                dset = file.create_dataset('object', (recon['Rtime_num'][i], recon['N_z'], recon['N_xy'], recon['N_xy']), dtype=np.float32, chunks=True);
-                dset_off = file.create_dataset('proj_offset', (proj['recon_N_r'], proj['recon_N_t']), dtype=np.float32, chunks=True)
 		for k in range(recon['node_num']):
 			temp = RealData4mBin(path2launch + 'proj_offset_n' + str(k) + '.bin', 0, proj['recon_N_r']*proj['recon_N_t']/recon['node_num'])
                 	proj_offset = temp.reshape((proj['recon_N_r'], proj['recon_N_t']/recon['node_num']), order = 'C')
-                	dset_off[:,k*proj['recon_N_t']/recon['node_num']:(k+1)*proj['recon_N_t']/recon['node_num']] = proj_offset.astype(np.float32)
+                	offset[:,k*proj['recon_N_t']/recon['node_num']:(k+1)*proj['recon_N_t']/recon['node_num']] = proj_offset.astype(np.float32)
                 for j in range(int(recon['Rtime_num'][i])):
+                	file = h5py.File(path2results + 'object_time_' + str(j) + '.hdf5', 'w');
+#               	dset = file.create_dataset('object', (recon['Rtime_num'][i], recon['N_z'], recon['N_xy'], recon['N_xy']), dtype=np.float32, chunks=True, compression='lzf');
+                	dset = file.create_dataset('object', (recon['N_z'], recon['N_xy'], recon['N_xy']), dtype=np.float32, chunks=True);
 			zpernode = recon['N_z']/recon['node_num']	
 			for k in range(recon['node_num']):
 				temp = RealData4mBin(path2launch + 'object_n' + str(k) + '_time_' + str(j) + '.bin', 0, recon['N_xy']*recon['N_xy']*zpernode)
                         	Object[k*zpernode:(k+1)*zpernode,:,:] = temp.reshape((zpernode, recon['N_xy'], recon['N_xy']), order = 'C')
 
                         Object[Object < 0] = 0
-                        dset[j,:,:,:] = Object.astype(np.float32)
-                file.close
+                        dset = Object.astype(np.float32)
+                	if (j == 0):
+				dset_off = file.create_dataset('proj_offset', (proj['recon_N_r'], proj['recon_N_t']), dtype=np.float32, chunks=True)
+				dset_off = offset.astype(np.float32)
+                	file.close
 
 def write_Video4mArray(filename, rows, cols, fps, array):
 	sz = array.shape
@@ -125,19 +127,19 @@ def write_Video4mArray(filename, rows, cols, fps, array):
 
 def RealData2Bin(filename, data):
 	fid = open(filename, 'wb')
-	#data = data.astype(np.float64)
-	data = data.astype(np.float32)
+	data = data.astype(np.float64)
+	#data = data.astype(np.float32)
 	data.tofile(fid)
 	fid.close()
 		
 def RealData4mBin(filename, offset, size):
 	fid = open(filename, 'rb')
-	fid.seek(offset*4, 0)
-	#fid.seek(offset*8, 0)
+	#fid.seek(offset*4, 0)
+	fid.seek(offset*8, 0)
 	# Multiply above offset by 8 which is size of double in bytes
-	data = fid.read(size*4)
-	#data = fid.read(size*8)
-	data = np.array(struct.unpack(str(size)+'f',data))	
-	#data = np.array(struct.unpack(str(size)+'d',data))	
+	#data = fid.read(size*4)
+	data = fid.read(size*8)
+	#data = np.array(struct.unpack(str(size)+'f',data))	
+	data = np.array(struct.unpack(str(size)+'d',data))	
 	fid.close()
 	return data

4D_Recon_Launcher_for_Real_Data/XT_Initialize.py
@@ -62,11 +62,11 @@ def proj_init (proj, args):
 	proj['use_slice_white'] = -1
 	proj['length_r'] = proj['voxel_size']*proj['N_r']	
 	proj['length_t'] = proj['voxel_size']*proj['N_t']	
-	if (args.SIM_DATA):
-		proj['length_t'] = proj['voxel_size']*proj['N_t']*(proj['N_r']/proj['recon_N_r'])
+#	if (args.SIM_DATA):
+#		proj['length_t'] = proj['voxel_size']*proj['N_t']*(proj['N_r']/proj['recon_N_r'])
 	#Above hack is to ensure square voxel sizes during reconstruction. Fix the hack sometime
 	proj['L'] = proj['N_theta']/proj['K']
-	proj['N_p'] = ((proj['proj_start'] + proj['proj_num'])/proj['N_theta'] + 1)*proj['N_theta']
+	proj['N_p'] = proj['N_theta']*args.num_cycles
 	
 	fps_of_camera = proj['L']/(180.0/proj['rotation_speed'])
 	angles, times = gen_interlaced_views_0_to_Inf(proj['K'], proj['N_theta'], proj['N_p'])
@@ -76,7 +76,7 @@ def proj_init (proj, args):
 
 	proj['angles'] = angles_clip[proj['proj_start'] : proj['proj_start'] + proj['proj_num']]
 	proj['times'] = times_clip[proj['proj_start'] : proj['proj_start'] + proj['proj_num']]
-	
+
 	proj['recon_N_p'] = len(proj['angles']) 	
 	print 'proj_init: Total number of projections used for reconstruction is ' + str(proj['recon_N_p'])
 	
@@ -172,11 +172,13 @@ def recon_init (proj, recon, args):
 	recon['initICD'] = 3*np.ones(recon['multres_z'])
 	recon['initICD'][0] = 2
 	recon['initICD'] = np.concatenate((2*np.ones(recon['multres_xy']-recon['multres_z']), recon['initICD']), axis=0)
+	recon['initICD'][0] = 0
 	if (args.restart):
-		recon['initICD'][0] = 1
 		args.run_setup = False
+		recon['multstart'] = args.restart_stage - 1
 	else:
-		recon['initICD'][0] = 0 
+		recon['multstart'] = 0
+		
 	recon['WritePerIter'] = 0*np.ones(recon['multres_xy'])
 	recon['updateProjOffset'] = 3*np.ones(recon['multres_xy'])
 	recon['updateProjOffset'][0] = 0
@@ -274,7 +276,10 @@ def recon_init (proj, recon, args):
 def files_init (files, args):
 	files['Launch_Folder'] = args.run_folder + '/XT_run/'
 	files['Result_Folder'] = args.run_folder + '/XT_Results/'
-	files['C_Source_Folder'] = "../Source_Code_4D_Fast/"
+	if (args.REAL_DATA):
+		files['C_Source_Folder'] = "../Source_Code_4D_APS2014/"
+	else:
+		files['C_Source_Folder'] = "../Source_Code_4D_Fast/"
 	files['Proj_Offset_File'] = "../Source_Code_4D/proj_offset.bin"
 	files['Converged_Object'] = args.converged_object_file
 	files['copy_executables'] = 0