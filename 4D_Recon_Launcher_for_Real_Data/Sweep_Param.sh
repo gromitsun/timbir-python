d6b0a6ab94a59524a97327c7ae8319c6599ee2e6
Kadri Aditya Mohan <mohank@conte-fe01.rcac.purdue.edu>
2014-08-17 16:14:05 -0400

    Changes to submit file (conte) for phase contrast dataset#60

--- 4D_Recon_Launcher_for_Real_Data/Sweep_Param.sh
+++ 4D_Recon_Launcher_for_Real_Data/Sweep_Param.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -l
 
-param_idx=`seq 0 1`
+param_idx=`seq 0 11`
 #$msg="data_20_lowres"
-export MSG="zeromean_"
+export MSG="alsi-60-"
 for i in $param_idx; do
 	export PARAM_INDEX=$i
-#	qsub -N $MSG$i -v PARAM_INDEX Conte_RealData_Submit.sh
-	qsub -N $MSG$i -v PARAM_INDEX Conte_SimData_Submit.sh 
+	qsub -N $MSG$i -v PARAM_INDEX Conte_RealData_Submit.sh
+#	qsub -N $MSG$i -v PARAM_INDEX Conte_SimData_Submit.sh 
 	sleep 0.2
 done 
